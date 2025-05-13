3fd7b62d3a682fad87607ecf8e3485f70060f602
bajwa315 <cartyrerepair3@gmail.com>
2025-05-13 11:18:41 -0700

    layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -56,16 +56,6 @@ export default function RootLayout({ children }) {
         />
         <meta name='robots' content='index, follow' />
 
-            <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=AW-17036263320"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'AW-17036263320');
-</script>
-  
         {/* Google Tag Manager */}
         <script
           dangerouslySetInnerHTML={{
